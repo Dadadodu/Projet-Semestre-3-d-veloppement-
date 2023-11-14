f40aed9ba18b1199b354d5012b2c265ae1303a88
Tao <tao.marissal@gmail.com>
2023-11-14 20:16:55 +0100

    premier commit

---
+++ Projet-jeu/index.js
@@ -0,0 +1,7 @@
+console.log("test");
+let tao = new Joueurs('Tao');
+console.log(tao.name);
+
+const canvas = document.querySelector(".class-canvas");
+//const width = (canvas.width = window.innerWidth);
+//const height = (canvas.height = window.innerHeight);
---
+++ Projet-jeu/joueur.js
@@ -0,0 +1,11 @@
+class Joueurs {
+
+    constructor(firstname){
+        this.firstname = firstname;
+        this.pos = [2,2];
+    }
+
+    get name(){
+        return this.firstname;
+    }
+}

f164002e04ea965349796dea57a4cbe2083d1b30
Marissal Tao <82579144+TaoMarissal@users.noreply.github.com>
2023-11-15 23:39:18 +0100

    commit du 15/11/2023

--- Projet-jeu/index.js
+++ Projet-jeu/index.js
@@ -1,7 +1,61 @@
-console.log("test");
-let tao = new Joueurs('Tao');
-console.log(tao.name);
+let joueur = new Joueurs('Tao');
+const TailleBloc = 50;
 
-const canvas = document.querySelector(".class-canvas");
-//const width = (canvas.width = window.innerWidth);
-//const height = (canvas.height = window.innerHeight);
+function graphisme(){
+    var canvas = document.querySelector('.canvas');
+    var context = canvas.getContext('2d');
+
+    // Clear the canvas before drawing
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    // Draw a red square (cube) of size 50x50 at coordinates (275, 275)
+    for (let i = 0; i < map[joueur.MapX][joueur.MapY].length; i++) {
+        for (let j = 0; j < map[joueur.MapX][joueur.MapY][i].length; j++) {
+            if(map[joueur.MapX][joueur.MapY][i][j]==1){
+                context.fillStyle = 'red';
+                context.fillRect(j*TailleBloc, i*TailleBloc, 50, 50);
+            }
+            if(map[joueur.MapX][joueur.MapY][i][j]==2){
+                context.fillStyle = 'blue';
+                context.fillRect(j*TailleBloc, i*TailleBloc, 50, 50);
+            }
+        }
+    }
+    context.fillStyle = 'green';
+    context.fillRect(joueur.pos[0]*TailleBloc, joueur.pos[1]*TailleBloc, 50, 50);
+}
+graphisme();
+
+document.addEventListener('keyup', function(event) {
+    const keyCode = event.code;
+    switch (keyCode) {
+        case 'KeyW':
+            joueur.deplacement('Z');
+            break;
+        case 'KeyA':
+            joueur.deplacement('Q');
+            break;
+        case 'KeyS':
+            joueur.deplacement('S');
+            break;
+        case 'KeyD':
+            joueur.deplacement('D');
+            break;
+        default:
+            break;
+    }
+    console.log("ceci est prepos "+joueur.prepos[0] +" "+joueur.prepos[1]);
+    if(joueur.verif()){
+        joueur.pos[0] = joueur.prepos[0];
+        joueur.pos[1] = joueur.prepos[1];
+    }else{
+        joueur.prepos[0] = joueur.pos[0];
+        joueur.prepos[1] = joueur.pos[1];
+    }
+
+    graphisme();
+});
+
+
+// console.log("ceci est prepos "+joueur.prepos[0] +" "+joueur.prepos[1]);
+// console.log("ceci est pos "+joueur.pos[0] +" "+joueur.pos[1]);
--- Projet-jeu/joueur.js
+++ Projet-jeu/joueur.js
@@ -2,10 +2,143 @@ class Joueurs {
 
     constructor(firstname){
         this.firstname = firstname;
-        this.pos = [2,2];
+        this.pos = [1,1];//[x][y]
+        this.prepos = [1,1];//[x][y]
+        this.MapX = 0;
+        this.MapY = 0;
     }
 
-    get name(){
-        return this.firstname;
+    deplacement(saisi) {
+        if (saisi === 'Z') {
+                this.prepos[1] -= 1;
+        } else if (saisi === 'D') {
+                this.prepos[0] += 1;
+        } else if (saisi === 'S') {
+                this.prepos[1] += 1;
+        } else if (saisi === 'Q') {
+                this.prepos[0] -= 1;
+        };
+    };
+
+    verif() {
+        if(map[joueur.MapX][joueur.MapY][joueur.prepos[1]][joueur.prepos[0]]==1){//map[id map][y][x]
+            console.log("test");
+            return 0;
+        }
+        return 1;
     }
-}
+
+
+};
+
+   
+
+// class Joueur {
+//     constructor() {
+//         this.pos = [2, 2];
+//         this.prepos = [2, 2];
+//         this.change = 0; // 1nord 2ouest 3sud 4est
+//         this.graph = 'mur4.png';
+//     }
+
+//     preposition(entre) {
+//         if (entre === 'Z') {
+//             if (this.prepos[0] === 0) {
+//                 this.prepos[0] = 10;
+//                 this.change = 1;
+//             } else {
+//                 this.prepos[0] -= 1;
+//             }
+//         } else if (entre === 'S') {
+//             if (this.prepos[0] === 10) {
+//                 this.prepos[0] = 0;
+//                 this.change = 3;
+//             } else {
+//                 this.prepos[0] += 1;
+//             }
+//         } else if (entre === 'Q') {
+//             if (this.prepos[1] === 0) {
+//                 this.prepos[1] = 10;
+//                 this.change = 4;
+//             } else {
+//                 this.prepos[1] -= 1;
+//             }
+//         } else if (entre === 'D') {
+//             if (this.prepos[1] === 10) {
+//                 this.prepos[1] = 0;
+//                 this.change = 2;
+//             } else {
+//                 this.prepos[1] += 1;
+//             }
+//         }
+//         return this.prepos;
+//     }
+
+//     viewPos() {
+//         return this.pos;
+//     }
+
+//     viewPrePos() {
+//         return this.prepos;
+//     }
+
+//     echec(direction) {
+//         if (direction === 'Z') {
+//             if (this.prepos[0] === 10) {
+//                 this.prepos[0] = 0;
+//             } else {
+//                 this.prepos[0] += 1;
+//             }
+//         } else if (direction === 'S') {
+//             if (this.prepos[0] === 0) {
+//                 this.prepos[0] = 10;
+//             } else {
+//                 this.prepos[0] -= 1;
+//             }
+//         } else if (direction === 'Q') {
+//             if (this.prepos[1] === 10) {
+//                 this.prepos[1] = 0;
+//             } else {
+//                 this.prepos[1] += 1;
+//             }
+//         } else if (direction === 'D') {
+//             if (this.prepos[1] === 0) {
+//                 this.prepos[1] = 10;
+//             } else {
+//                 this.prepos[1] -= 1;
+//             }
+//         }
+//         return this.prepos;
+//     }
+
+//     deplacement(saisi) {
+//         if (saisi === 'Z') {
+//             if (this.pos[1] === 0) {
+//                 this.pos[1] = 10;
+//             } else {
+//                 this.pos[1] -= 1;
+//             }
+//         } else if (saisi === 'D') {
+//             if (this.pos[0] === 10) {
+//                 this.pos[0] = 0;
+//             } else {
+//                 this.pos[0] += 1;
+//             }
+//         } else if (saisi === 'S') {
+//             if (this.pos[1] === 10) {
+//                 this.pos[1] = 0;
+//             } else {
+//                 this.pos[1] += 1;
+//             }
+//         } else if (saisi === 'Q') {
+//             if (this.pos[0] === 0) {
+//                 this.pos[0] = 10;
+//             } else {
+//                 this.pos[0] -= 1;
+//             }
+//         }
+//     }
+// }
+
+// // Example usage:
+// const joueur = new Joueur();
